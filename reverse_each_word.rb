5fb98ca71f847c09ff887380e671bcec1a14c016
Freda Hon <freda.hon@gmail.com>
2020-09-26 16:57:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,12 @@ require 'pry'
 
 # First solve it using .each
 def reverse_each_word(string)
-  new_array = []
-  string_array = string.split(" ")
+  string_array = string.split
+  new_array = [ ]
   string_array.each do |word|
-    new_array << word.reverse
+    new_array.push(word.reverse)
   end
-  new_array.join("")
+  new_array.join(" ")
 end
 
 # Then utilize the same method using .collect to see the difference.
