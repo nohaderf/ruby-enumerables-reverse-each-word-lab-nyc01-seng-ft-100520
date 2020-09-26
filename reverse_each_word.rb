e551c53454a7edfbc27ebcec92965fe3c94f3c84
Freda Hon <freda.hon@gmail.com>
2020-09-26 16:52:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,13 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  string_array = string.split
+  new_array = [ ]
+  string_array.each do |word|
+    new_array.push(word.reverse)
+  end
+  new_array.join(" ")
+end
+
