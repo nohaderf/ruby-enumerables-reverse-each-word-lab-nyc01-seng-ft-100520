90dccf403335b2cebc5ba439ca56d1c0a921525b
Freda Hon <freda.hon@gmail.com>
2020-09-26 16:51:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
-def reverse_each_word(string)
-  string_array = string.split
-  new_array = [ ]
-  string_array.each do |word|
-    new_array.push(word.reverse)
-  end
-  new_array.join(" ")
-end
-
 
 def reverse_each_word(string)
   string_array = string.split(" ")
