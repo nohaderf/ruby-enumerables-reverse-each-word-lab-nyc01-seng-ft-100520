43be45cd53f5671bd5d0363880f39891b111cfbd
Freda Hon <freda.hon@gmail.com>
2020-09-26 16:53:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,17 @@ require 'pry'
 
 # Write a method called reverse_each_word that takes in a string 
 
+# First solve it using .each
+def reverse_each_word(string)
+  string_array = string.split
+  new_array = [ ]
+  string_array.each do |word|
+    new_array.push(word.reverse)
+  end
+  new_array.join(" ")
+end
+
+
 def reverse_each_word(string)
   string_array = string.split(" ")
   new_array = [ ]
@@ -11,11 +22,3 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
-def reverse_each_word(string)
-  string_array = string.split
-  new_array = [ ]
-  string_array.each do |word|
-    new_array.push(word.reverse)
-  end
-  new_array.join(" ")
-end
