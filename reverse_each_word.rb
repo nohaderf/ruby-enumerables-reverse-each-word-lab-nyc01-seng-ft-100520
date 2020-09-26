76cf999a79a3bcf6c503477f435718807f5b38b2
Freda Hon <freda.hon@gmail.com>
2020-09-26 16:53:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
-
+# Then utilize the same method using .collect to see the difference.
 def reverse_each_word(string)
   string_array = string.split(" ")
   new_array = [ ]
